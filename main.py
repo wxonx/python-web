b4d2646c81ba3c597362c68665f0f6289c24a1f5
wxonx <xwonx1@gmail.com>
2020-07-19 15:22:31 +0000

    new fucn

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages
 
-indeed = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+max_indeed_pages = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed.text, "html.parser")
-
-pagination = indeed_soup.find("div", {"class": "pagination"})
-
-pages = pagination.find_all('a')
-spans = []
-for page in pages[:-1]:
-    spans.append(int(page.string))
-
-max_page = spans[-1]
-
-
-for n in range(max_page):
-    print(f"start={n*50}")
+print(max_indeed_pages)
