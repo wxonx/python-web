7928354fbd20b66d48ca6a7203a0f6e4db423e33
wxonx <xwonx1@gmail.com>
2020-07-21 14:23:02 +0000

    company list extract

--- indeed.py
+++ indeed.py
@@ -22,8 +22,14 @@ def extract_indeed_jobs(last_page):
   result = requests.get(f"{url}&start={0*limit}")
   soup = BeautifulSoup(result.text, "html.parser")
   results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+  #find.all = 리스트 전부를 가져옴
+  #find는 첫번째 찾은 결과를 보여줌
   for result in results:
       title = result.find("h2", {"class": "title"}).find("a")["title"]
-      print(title)
+      company  = result.find("span", {"class": "company"})
+      if company.find("a") is not None:
+        print(company.find("a").string)
+      else:
+        print(company.string)   
   return jobs
 
