33506106de8a81bd8fd62f504a7dab0ecb89ebda
wxonx <xwonx1@gmail.com>
2020-07-21 14:31:18 +0000

    space del, title&company list

--- indeed.py
+++ indeed.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-limit = 1000
+limit = 500
 url = f"https://www.indeed.com/jobs?q=python&limit={limit}"
 
 def extract_indeed_pages():
@@ -27,9 +27,12 @@ def extract_indeed_jobs(last_page):
   for result in results:
       title = result.find("h2", {"class": "title"}).find("a")["title"]
       company  = result.find("span", {"class": "company"})
-      if company.find("a") is not None:
-        print(company.find("a").string)
+      company_anchor = company.find("a")
+      if company_anchor is not None:
+        company = (str(company_anchor.string))
       else:
-        print(company.string)   
+        company = (str(company.string))   
+      company = company.strip()
+      print(title, company)
   return jobs
 
