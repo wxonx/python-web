6a6ed7643717a07e1ebbeb242aaa10605b4fe7e1
wxonx <xwonx1@gmail.com>
2020-07-26 12:10:38 +0000

    extract jobs

--- stackoverflow.py
+++ stackoverflow.py
@@ -11,12 +11,26 @@ def get_last_page():
   result = requests.get(url)
   soup = BeautifulSoup(result.text, "html.parser")
   pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
-  print(pages)
+  last_page = pages[-2].get_text(strip=True)#whitespace del
+  return int(last_page)
+
+
+def extract_jobs(last_page):
+  jobs =[]
+  for page in range(last_page):
+    result = requests.get(f"{url}&pg={page+1}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class":"-job"})
+    for result in results:
+      print(result["data-jobid"])
+
+    
 
 
 
 def get_jobs():
   last_page = get_last_page()
-  return[]
+  jobs = extract_jobs(last_page)
+  return jobs
   
 
